7523f8df623af01e7318c4b765535e7992546be9
Alexander <caminante.msk@gmail.com>
2023-03-22 11:08:55 -0300

    fixing home mobile layout

--- src/index.js
+++ src/index.js
@@ -1,49 +1,19 @@
 import './styles/index.scss'
-// import headerWithScroll from './components/header/headerWithScroll'
-// import home from './components/home/home'
-// import discount from './components/discount/discount'
-// import portfolio from './components/portfolio/portfolio'
-// import langToggle from './components/langToggle/langToggle'
-// import offers from './components/offers/offers'
-// import blogBox from './components/blogBox/blogBox'
 import antiFouc from './antiFouc'
 import yall from 'yall-js'
-// import blogHeader from './components/blogHeader/blogHeader'
+import resizeAnimation from './resizeAnimation'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 import 'animate.css'
-// import blogPost from './components/blogPost/blogPost'
-// import instaFeed from './components/instaFeed/instaFeed'
 import header from './sections/header/header'
 import opinions from './sections/opinions/opinions'
 import contact from './sections/contact/contact'
-// import office from './sections/office/office'
-
 
 AOS.init()
 document.addEventListener("DOMContentLoaded", yall)
-// if (window.location.href.includes('blog.html')) {
-//   // blogHeader()
-//   // blogBox()
-// } else if (window.location.href.includes('post.html')) {
-//   // blogHeader()
-//   // blogPost()
-// } else {
-//   localStorage.clear()
-//   // headerWithScroll()
-//   // home()
-//   // discount()
-//   // portfolio()
-//   // offers()
-//   // // langToggle()
-//   // instaFeed()
-// }
-
-// office()
-
-antiFouc()
 header()
 opinions()
 contact()
-
+resizeAnimation()
+antiFouc()
 
